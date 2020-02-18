ae5a72ff0511dd748f4e8c36ec9ad16023c7375f
chrisawesomeusername <chrisawesomeusername@gmail.com>
2020-02-18 15:26:43 +1300

    t

--- js/script.js
+++ js/script.js
@@ -4,4 +4,28 @@ $(document).ready(function(){
   $('body').click(function(){
   $(this).css('color','white');
 });
+
+
+var myKey = JSON.parse(apiKey);
+console.log(myKey[0]);
+myKey = myKey[0].key;
+console.log(myKey);
+
+  // $.ajax({
+  //   url : `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${myKey}`,
+  //   type :'GET',
+  //   data :'json',
+  //   success:function(data){
+  //     console.log(data);
+  //     var i;
+  //     for (i = 0; i < data.photos.length; i++) {
+  //
+  //
+  //     }
+  //   },
+  //   error:function(){
+  //   console.log('error');
+  //   }
+  //
+  // });//ajax
 });
