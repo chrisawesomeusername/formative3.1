f64d4803f49dc3d962d5d8b1dd1ce6042b09d6f9
chrisawesomeusername <chrisawesomeusername@gmail.com>
2020-02-19 15:20:12 +1300

    cards filled with content from api

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,18 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //   },
-    //   build: {
-    //     src: 'src/<%= pkg.name %>.js',
-    //     dest: 'build/<%= pkg.name %>.min.js'
-    //   }
-    // }
+    uglify: {
+      build: {
+        src: 'js/script.js',
+        dest: 'js/script.min.js'
+      }
+    }, // end uglify
+    watch: {
+      all: {
+        files: ['sass/style.scss','css/style.css','js/script.js'],
+        tasks: ['sass','csslint','jshint']
+      },
+    },// end watch
     sass: {                              // Task
       dist: {                            // Target
         options: {                       // Target options
@@ -21,13 +24,38 @@ module.exports = function(grunt) {
           'css/style.css': 'sass/style.scss'       // 'destination': 'source'
         }
       }
-    }
+    }, //end sass
+    csslint: {
+      strict: {
+        options: {
+          import: 2
+        },
+        src: ['css/style.css']
+      },
+      lax: {
+        options: {
+          import: false,
+          'order-alphabetical' :false
+        },
+        src: ['css/style.css']
+      }
+    }, //end css lint
+    jshint: {
+      all: ['Gruntfile.js', 'js/script.js']
+    } // end jshint
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load the plugin that provides the task(s).
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // grunt.loadNpmTasks('grunt-w3c-html-validation');
 
   // Default task(s).
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('ugly', ['uglify']);//To run in terminal: grunt ugly
+  grunt.registerTask('default', ['watch']);//To run in terminal: grunt
 
 };
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-console.log('hello');
+/* jshint esversion: 6 */
+
+console.log('formative3.1 news api');
 
 $(document).ready(function(){
   $('body').click(function(){
@@ -7,25 +9,32 @@ $(document).ready(function(){
 
 
 var myKey = JSON.parse(apiKey);
-console.log(myKey[0]);
+// console.log(myKey[0]);
 myKey = myKey[0].key;
 console.log(myKey);
 
-  // $.ajax({
-  //   url : `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${myKey}`,
-  //   type :'GET',
-  //   data :'json',
-  //   success:function(data){
-  //     console.log(data);
-  //     var i;
-  //     for (i = 0; i < data.photos.length; i++) {
-  //
-  //
-  //     }
-  //   },
-  //   error:function(){
-  //   console.log('error');
-  //   }
-  //
-  // });//ajax
+  $.ajax({
+    url : `http://newsapi.org/v2/everything?q=bitcoin&apiKey=${myKey}`,
+    type :'GET',
+    data :'json',
+    success:function(data){
+      console.log(data);
+      var i;
+      for (i = 0; i < data.articles.length; i++) {
+        document.getElementById('result').innerHTML +=
+        '<div class="card-columns">' +
+          '<div class="card">' +
+            '<img src="' + data.articles[i].urlToImage + '" class="card-img-top" alt="..">' +
+            '<div class="card-body">' +
+            '<h5 class="card-title">' + data.articles[i].title + '</h5>' +
+            '<p class="card-text">' + data.articles[i].description + '</p>' +
+          '</div>' +
+        '</div>';
+      }
+    },
+    error:function(){
+    console.log('error');
+    }
+
+  });//ajax
 });
